2f62c0be1d984ea6b7dbb73e9c5f8c81d07d7395
shyam <shyamayadav154@gmail.com>
2023-06-07 20:00:58 +0530

    update: font updated to inter

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import "nextra-theme-docs/style.css";
-import '../style.css'
+import "../style.css";
+import { Inter} from "@next/font/google";
+
+const inter= Inter({
+    subsets: ["latin"],
+});
 
 export default function Nextra({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <main className={inter.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
