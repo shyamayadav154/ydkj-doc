de325be7f97b87e2c2d674d8407ac5c55d41c6cf
shyam <shyamayadav154@gmail.com>
2023-06-07 10:02:09 +0530

    feat: added metadata and favicon

--- theme.config.js
+++ theme.config.js
@@ -1,4 +1,6 @@
 /** @type {import('nextra-theme-docs').DocsThemeConfig} */
+import { useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const themConfig = {
     logo: () => (
@@ -26,6 +28,59 @@ const themConfig = {
     },
     sidebar: {
         defaultMenuCollapseLevel: 1,
+    },
+    head: () => {
+        const { route } = useRouter();
+        const { frontMatter, title } = useConfig();
+        const titleSuffix = "YDKJ Docs";
+        const description = "You Don't Know JS 1st Ed. - Docs";
+        const imageUrl = new URL("https://ydkj-doc.vercel.app/logo.png");
+
+        if (!/\/index\.+/.test(route)) {
+            imageUrl.searchParams.set("title", title);
+        }
+        const ogTitle = title ? `${title} – YDKJ Docs` : `YDKJ Docs: ${titleSuffix}`;
+        const ogDescription = frontMatter.description || description;
+        const ogImage = frontMatter.image || imageUrl.toString();
+
+        return (
+            <>
+                {/* Favicons, meta */}
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicon/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon/favicon-16x16.png"
+                />
+                <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+                <link
+                    rel="mask-icon"
+                    href="/favicon/safari-pinned-tab.svg"
+                    color="#000000"
+                />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="apple-mobile-web-app-title" content="YDKJ - Docs" />
+                <meta name="description" content={ogDescription} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@vercel" />
+                <meta name="twitter:image" content={ogImage} />
+                <meta property="og:title" content={ogTitle} />
+                <meta property="og:description" content={ogDescription} />
+                <meta property="og:image" content={ogImage} />
+            </>
+        );
     },
 };
 
