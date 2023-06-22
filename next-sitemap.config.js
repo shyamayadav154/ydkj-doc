ac75583907ea1bdc4ce5e2118f8b344ba3ddbc4e
shyam <shyamayadav154@gmail.com>
2023-06-22 16:38:07 +0530

    fix: url env

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.VERCEL_URL || 'https://ydkj-doc.vercel.app/',
+  siteUrl: process.env.VERCEL_BRANCH_URL || 'https://ydkj-doc.vercel.app/',
   generateRobotsTxt: true, // (optional)
 }
